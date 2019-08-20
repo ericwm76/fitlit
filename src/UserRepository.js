a84c60695708193ba564b926fdf6e5b75a58c208
Eric Meldrum <eric.meldrum@gmail.com>
2019-08-19 21:56:40 -0600

    Create tests for UserRepository class; add methods to UserRepository class

---
+++ src/UserRepository.js
@@ -0,0 +1,16 @@
+class UserRepository {
+  constructor(data) {
+    this.data = data;
+  }
+
+  returnUserData(userID) {
+    return this.data.find(user => user.id === userID);
+  }
+
+  avgStepGoal() {
+    let stepTotal = this.data.reduce((prev, curr) => prev + curr.dailyStepGoal, 0);
+    return stepTotal / this.data.length;
+  }
+}
+
+module.exports = UserRepository;
--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -4,20 +4,27 @@ const expect = chai.expect;
 const UserRepository = require('../src/UserRepository');
 const sampleUserData = require('../test/users-sample');
 
-describe('UserRepository', function() {
-  it('should ensure testing is working', function() {
-    expect(true).to.equal(true)
+describe('UserRepository', () => {
+  it('should ensure testing is working', () => {
+    expect(true).to.equal(true);
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
 
-  it('should return user data given a user id', function() {
-
+  it('should hold user data', () => {
+    let userRepo = new UserRepository(sampleUserData);
+    expect(userRepo.data).to.equal(sampleUserData);
   });
 
-  it('should calculate the average step goal of all users', function() {
+  it('should return user data given a user id', () => {
+    let userRepo = new UserRepository(sampleUserData);      
+    expect(userRepo.returnUserData(3)).to.eql(sampleUserData[2]);
+  });
 
+  it('should calculate the average step goal of all users', () => {
+    let userRepo = new UserRepository(sampleUserData);
+    expect(userRepo.avgStepGoal()).to.equal(6400);
   });
 });  
