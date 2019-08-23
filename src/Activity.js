1af6db019b8a000b2279f000eae8fc043361dd2d
Eric Meldrum <eric.meldrum@gmail.com>
2019-08-23 00:05:54 -0600

    Refactor all methods and tests for Activity class to make code more modular and reusable

--- src/Activity.js
+++ src/Activity.js
@@ -11,61 +11,100 @@ class Activity {
     return this.data.filter(obj => obj.date === date)
   }
 
-  findPropertyArray(id, property) {
-    let activityData = this.getDataByID(id);
-    return activityData.map(data => data[property]);
+  findPropArray(id, prop) {
+    let dataObjs = this.getDataByID(id);
+    return dataObjs.map(data => data[prop]);
   }
 
-  findMilesWalked(id, date, stride) {
-    let stepsByDate = this.getDataByDate(date).find(obj => obj.userID === id).numSteps;
+  // findMilesWalked(id, date, stride) {
+  //   let stepsByDate = this.getDataByDate(date).find(obj => obj.userID === id).numSteps;
 
-    return Math.round(((stepsByDate * stride) / 5280) * 100) / 100; 
+  //   return Math.round(((stepsByDate * stride) / 5280) * 100) / 100; 
+  // }
+
+  findSingleValue(id, date, prop) {
+    return this.getDataByID(id).find(dataObj => dataObj.date === date)[prop];
   }
 
-  findMinutesActive(id, date) {
-    return this.getDataByDate(date).find(obj => obj.userID === id).minutesActive;
+  findMilesWalked(id, date, prop, stride) {
+    let steps = this.findSingleValue(id, date, prop);
+    return Math.round(((steps * stride) / 5280) * 100) / 100;
   }
 
-  findWeeklyMinutesActive(id, date) {
-    let minutesArray = this.findPropertyArray(id, 'minutesActive');
-    if (minutesArray.length >= 7) {
-      return minutesArray.slice(minutesArray.length - 7);
+  // This should all be done by findSingleValue()
+    // findMinutesActive(id, date) {
+    //   return this.getDataByDate(date).find(obj => obj.userID === id).minutesActive;
+    // }
+
+  findWeekArray(id, date, prop) {
+    let propArray = this.findPropArray(id, prop);
+    let dateIndex = this.getDataByID(id).findIndex(dataObj => dataObj.date === date);
+
+    if (propArray.length >= 7) {
+      return propArray.slice(dateIndex - 6, dateIndex + 1);
     } else {
-      return minutesArray;
+      return propArray;
     }
   }
 
-  findAvgWeeklyMinutesActive(id, date) {
-    return Math.round(this.findWeeklyMinutesActive(id, date).reduce((prev, curr) => prev += curr) / 7) 
+  // findWeeklyMinutesActive(id, date) {
+  //   let minutesArray = this.findPropertyArray(id, 'minutesActive');
+  //   if (minutesArray.length >= 7) {
+  //     return minutesArray.slice(minutesArray.length - 7);
+  //   } else {
+  //     return minutesArray;
+  //   }
+  // }
+  findWeekAvg(id, date, prop) {
+    let weekArr = this.findWeekArray(id, date, prop);
+    return Math.round(this.findAvg(weekArr));
   }
-
-  hitStepGoal(id, date, goal) {
-    let stepsByDate = this.getDataByDate(date).find(obj => obj.userID === id).numSteps;
-
-    if (stepsByDate >= goal) {
-      return 'You hit your step goal!';
+  // findAvgWeeklyMinutesActive(id, date) {
+  //   return Math.round(this.findWeeklyMinutesActive(id, date).reduce((prev, curr) => prev += curr) / 7) 
+  // }
+  hitGoal(id, date, prop, goal) {
+    let steps = this.findSingleValue(id, date, prop);
+    if (steps >= goal) {
+      return 'You hit your goal!';
     } else {
       return 'Almost!';
     }
   }
+  // hitStepGoal(id, date, goal) {
+  //   let stepsByDate = this.getDataByDate(date).find(obj => obj.userID === id).numSteps;
 
-  hitWeeklyStepsGoals(id, date, goal) {
+  //   if (stepsByDate >= goal) {
+  //     return 'You hit your step goal!';
+  //   } else {
+  //     return 'Almost!';
+  //   }
+  // }
+
+  hitWeeklyGoals(id, date, goal) {
     let days = [];
     this.getDataByID(id).forEach(obj => {
       if (obj.numSteps >= goal) {
         days.push(obj.date);
       }
     })
-    return days
+    return days;
   }
 
-  findStairRecord(id, property) {
-    let stairsArray = this.findPropertyArray(id, property);
-    return Math.max(...stairsArray);
+  findMaxOneUser(id, prop) {
+    let propArray = this.findPropArray(id, prop);
+    return Math.max(...propArray);
   }
+  // findStairRecord(id, property) {
+  //   let stairsArray = this.findPropertyArray(id, property);
+  //   return Math.max(...stairsArray);
+  // }
 
+  // findAvg(array) {
+  //   return Math.round(array.reduce((prev, curr) => prev += curr) / array.length) 
+  // }
   findAvg(array) {
-    return Math.round(array.reduce((prev, curr) => prev += curr) / array.length) 
+    let avg = array.reduce(((prev, curr) => prev += curr), 0) / array.length;
+    return Math.round(avg);
   }
 }
 
--- test/activity-test.js
+++ test/activity-test.js
@@ -31,37 +31,36 @@ describe('Activity', () => {
   })
 
   it('should return the miles a user has walked based on their number of steps on a given day', () => {
-    expect(active.findMilesWalked(user.id, '2019/06/22', user.strideLength)).to.equal(3.07)
+    expect(active.findMilesWalked(user.id, '2019/06/22', 'numSteps', user.strideLength)).to.equal(3.07)
   })
 
   it('should return the number of minutes a user was active on a given day', () => {
-    expect(active.findMinutesActive(user.id, '2019/06/22')).to.equal(124);
+    expect(active.findSingleValue(user.id, '2019/06/22', 'minutesActive')).to.equal(124);
   })
 
   it('should return the number of minutes a user was active in a given week', () => {
-    expect(active.findWeeklyMinutesActive(user.id, '2019/06/22')).to.eql([220, 65, 181, 243, 74, 174, 124]);
+    expect(active.findWeekArray(user.id, '2019/06/22', 'minutesActive')).to.eql([220, 65, 181, 243, 74, 174, 124]);
   })
 
   it('should return the average minutes a user was active in a given week', () => {
-    expect(active.findAvgWeeklyMinutesActive(user.id, '2019/06/22')).to.equal(154);
+    expect(active.findWeekAvg(user.id, '2019/06/22', 'minutesActive')).to.equal(154);
   })
 
   it('should tell whether a user hit their step goal on a given day', () => {
-    expect(active.hitStepGoal(user.id, '2019/06/22', 3000)).to.equal('You hit your step goal!');
-    expect(active.hitStepGoal(user.id, '2019/06/22', user.dailyStepGoal)).to.equal('Almost!');
+    expect(active.hitGoal(user.id, '2019/06/22', 'numSteps', 3000)).to.equal('You hit your goal!');
+    expect(active.hitGoal(user.id, '2019/06/22', 'numSteps', user.dailyStepGoal)).to.equal('Almost!');
   })
 
   it('should find all the days where a user exceeded their step goal', () => {
-    expect(active.hitWeeklyStepsGoals(user.id, '2019/06/22', user.dailyStepGoal)).to.eql(['2019/06/17', '2019/06/19', '2019/06/20', '2019/06/21']);
+    expect(active.hitWeeklyGoals(user.id, '2019/06/22', user.dailyStepGoal)).to.eql(['2019/06/17', '2019/06/19', '2019/06/20', '2019/06/21']);
   })
 
   it('should find a user\'s all-time stair climbing record', () => {
-    expect(active.findStairRecord(user.id, 'flightsOfStairs')).to.equal(44)
+    expect(active.findMaxOneUser(user.id, 'flightsOfStairs')).to.equal(44)
   })
 
   it('should find the average number of stairs climbed for a specific date, all users', () => {
     expect(active.findAvg(active.getDataByDate('2019/06/22').map(obj => obj.flightsOfStairs))).to.equal(22)
-
   })
 
   it('should find the average number of steps taken for a specific date, all users', () => {
@@ -70,6 +69,5 @@ describe('Activity', () => {
 
   it('should find the average number of minutes active for a specific date, all users', () => {
     expect(active.findAvg(active.getDataByDate('2019/06/22').map(obj => obj.minutesActive))).to.equal(163)
-
   })
 })
