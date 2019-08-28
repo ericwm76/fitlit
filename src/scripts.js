2fc66390e21bc8aaf64d8988aa639de5f18821f7
Eric Meldrum <eric.meldrum@gmail.com>
2019-08-27 22:35:09 -0600

    Load icons into each card; change background color; populate text elements

--- src/scripts.js
+++ src/scripts.js
@@ -11,7 +11,26 @@ let activity = new Activity(activityData);
 
 $('#user-name').text(user1.returnFirstName());
 $('#todays-date').text(currentDate);
+$('#user-step-goal').text(user1.dailyStepGoal);
+$('#world-step-goal').text(userRepo.avgStepGoal());
 $('#ounces').text(hydration.findSingleValue(user1.id, currentDate, 'numOunces'));
+
+$('#hours').text(sleep.findSingleValue(user.id, currentDate, 'hoursSlept'));
+
+$('#hours-all-time').text(sleep.findAvg(sleep.findPropArray(user.id, 'hoursSlept')));
+$('#quality').text(sleep.findSingleValue(user.id, currentDate, 'sleepQuality'));
+
+$('#quality-all-time').text(sleep.findAvg(sleep.findPropArray(user.id, 'sleepQuality')));
+$('#steps').text(activity.findSingleValue(user.id, currentDate, 'numSteps'));
+$('#miles').text(activity.findMilesWalked(user.id, currentDate, 'numSteps', user1.strideLength))
+$('#stride-length').text(user1.strideLength)
+
+$('#steps-worldwide-avg').text(activity.findAvg(activity.getDataByDate(currentDate).map(obj => obj.numSteps)))
+$('#minutes').text(activity.findSingleValue(user.id, currentDate, 'minutesActive'));
+$('#minutes-worldwide-avg').text(activity.findAvg(activity.getDataByDate(currentDate).map(obj => obj.minutesActive)))
+$('#stairs').text(activity.findSingleValue(user.id, currentDate, 'flightsOfStairs'));
+
+$('#stairs-worldwide-avg').text(activity.findAvg(activity.getDataByDate(currentDate).map(obj => obj.flightsOfStairs)))
 
 
 
