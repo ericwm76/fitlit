a36400251224c1b977cc98fef4ad4eb47d193cf6
Eric Meldrum <eric.meldrum@gmail.com>
2019-08-29 07:42:59 -0600

    Create media query and adjust styling of buttons; add lunar calculations to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ let hydration = new Hydration(hydrationData);
 let sleep = new Sleep(sleepData);
 let activity = new Activity(activityData);
 let dates = activity.findWeekArray(user1.id, currentDate, 'date');
+let friendList = generateFriends(user1.friends)
 
 populateDates();
 populateWeekData(hydration, 'numOunces', 'ounces')
@@ -16,9 +17,9 @@ populateWeekData(activity, 'numSteps', 'steps')
 populateWeekData(activity, 'minutesActive', 'minutes')
 populateWeekData(activity, 'flightsOfStairs', 'stairs')
 
-$('#profile').click(function() {$('#user-info').toggle()
+$('#profile').click(function() {$('#aside-left').toggle()
 });
-$('#friends').click(function() {$('#friend-list').toggle()
+$('#friends').click(function() {$('#aside-right').toggle()
 });
 $('#full-name').text(user1.name)
 $('#address').text(user1.address)
@@ -27,6 +28,15 @@ $('#user-name').text(user1.returnFirstName());
 $('#todays-date').text(currentDate);
 $('#user-step-goal').text(user1.dailyStepGoal);
 $('#world-step-goal').text(userRepo.avgStepGoal());
+$('#friend-1-name').text(friendList[0].name)
+$('#friend-1-goal').text(friendList[0].stepGoal)
+$('#friend-1-steps').text(friendList[0].stepsThisWeek)
+$('#friend-2-name').text(friendList[1].name)
+$('#friend-2-goal').text(friendList[1].stepGoal)
+$('#friend-2-steps').text(friendList[1].stepsThisWeek)
+$('#friend-3-name').text(friendList[2].name)
+$('#friend-3-goal').text(friendList[2].stepGoal)
+$('#friend-3-steps').text(friendList[2].stepsThisWeek)
 $('#ounces').text(hydration.findSingleValue(user1.id, currentDate, 'numOunces'));
 $('#hours').text(sleep.findSingleValue(user.id, currentDate, 'hoursSlept'));
 $('#hours-all-time').text(sleep.findAvg(sleep.findPropArray(user.id, 'hoursSlept')));
@@ -40,6 +50,11 @@ $('#minutes').text(activity.findSingleValue(user.id, currentDate, 'minutesActive
 $('#minutes-worldwide-avg').text(activity.findAvg(activity.getDataByDate(currentDate).map(obj => obj.minutesActive)))
 $('#stairs').text(activity.findSingleValue(user.id, currentDate, 'flightsOfStairs'));
 $('#stairs-worldwide-avg').text(activity.findAvg(activity.getDataByDate(currentDate).map(obj => obj.flightsOfStairs)))
+$('#moon-distance').text(sleep.findSingleValue(user.id, currentDate, 'hoursSlept') * 2288);
+$('#moon-miles').text(Math.round(6786 / (activity.findMilesWalked(user.id, currentDate, 'numSteps', user1.strideLength))))
+$('#rocket-height').text(Math.round((activity.findSingleValue(user.id, currentDate, 'flightsOfStairs') / 34) * 100));
+
+
 
 function getRandomID () {
   return Math.floor(Math.random() * 50)
@@ -47,7 +62,7 @@ function getRandomID () {
 
 function populateDates () {
   dates.forEach((date, i) => {
-    $(`.date-${i + 1}`).text(dates[i])
+    $(`.date-${i + 1}`).text(dates[i] + ':')
   })
 }
 
@@ -73,10 +88,4 @@ function generateFriends (userFriends) {
   })
 
   return friends;
-}
-
-let friendList = generateFriends(user1.friends)
-
-function compareFriends () {
-
 }
